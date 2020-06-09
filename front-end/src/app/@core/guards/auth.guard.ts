1daa9285f9b94d46a79cd3b4afa18f4a2562ff01
mhmdwSamir <MhmdGezo1912@gmail.com>
2020-06-09 21:43:58 +0200

    Create auth guard to prevent user from opening pages that not authorized for him

---
+++ front-end/src/app/@core/guards/auth.guard.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, CanLoad, Route, UrlSegment, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { SignService } from 'src/app/signingService/sign.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(private _userService: SignService, private _router: Router) {}
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    if (this._activate()) {
+      return true;
+    } else {
+      this._router.navigate(['/access-denaied'])
+    };
+  }
+
+  private _activate() {
+    return this._userService.currentUser != undefined && this._userService.currentUser.username != undefined;
+  }
+}
--- front-end/src/app/signingService/sign.service.ts
+++ front-end/src/app/signingService/sign.service.ts
@@ -15,22 +15,21 @@ export interface userCredentials {
   providedIn: 'root',
 })
 export class SignService {
-  // rootUrl = '';
-  // currentUser = JSON.parse(localStorage.getItem('user')) as IUserAuthentication;
-  // constructor(private _http: HttpClient) {}
+  currentUser = JSON.parse(localStorage.getItem('user')) as IUserAuthentication;
 
-  // login(username: string, password: string): Observable<IUserAuthentication> {
-  //   if (password == 'admin' && username == 'admin') {
-  //     return of({ username, isAdmin: true });
-  //   } else if (password == 'P@ssw0rd' && username == 'mohammed') {
-  //     return of({ username, isAdmin: false });
-  //   } else {
-  //     return throwError({ error: 'unauthorized' });
-  //   }
-  // }
   rootUrl = `http://localhost:3000/api/auth`;
   constructor(private _http: HttpClient) {}
   createUser(userData: userCredentials) {
     return this._http.post(`${this.rootUrl}/signUp`, userData);
+  }
+  
+  login(username: string, password: string): Observable<IUserAuthentication> {
+    if (password == 'admin' && username == 'admin') {
+      return of({ username, isAdmin: true });
+    } else if (password == 'P@ssw0rd' && username == 'mohammed') {
+      return of({ username, isAdmin: false });
+    } else {
+      return throwError({ error: 'unauthorized' });
+    }
   }
 }
