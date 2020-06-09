69c7c0f97ef798dfb87d6f971380e9cd103b385f
mhmdwSamir <MhmdGezo1912@gmail.com>
2020-06-09 21:41:53 +0200

    Create access denied component and route

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -4,14 +4,24 @@ import { ListMoviesComponent } from './list-movies/list-movies.component';
 import { UpdateMovieComponent } from './update-movie/update-movie.component';
 import { CreateMovieComponent } from './create-movie/create-movie.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { AccessDenaiedComponent } from './sharing/components/access-denaied/access-denaied.component';
+import { AuthGuard } from './@core/guards/auth.guard';
 const routes: Routes = [
   { path: '', component: ListMoviesComponent },
   { path: ':id/update', component: UpdateMovieComponent },
-  { path: 'create', component: CreateMovieComponent },
+  {
+    path: 'create',
+    component: CreateMovieComponent,
+    canActivate: [AuthGuard]
+  },
   {
     path: 'settings',
     loadChildren: () =>
       import('./modules/modules.module').then((m) => m.ModulesModule),
+  },
+  {
+    path: 'access-denaied',
+    component: AccessDenaiedComponent
   },
   { path: '**', component: NotFoundComponent },
 ];
