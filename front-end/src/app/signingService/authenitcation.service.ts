f9e89e4eb35854df94cfb0bd5aba1832103b7c81
mhmdwSamir <mhmdgezo1912@gmail.com>
2020-06-10 19:57:12 +0200

    fix authorization bug

--- front-end/src/app/signingService/authenitcation.service.ts
+++ front-end/src/app/signingService/authenitcation.service.ts
@@ -5,7 +5,7 @@ import { tap } from 'rxjs/operators';
 class User {
   username: string;
   isAdmin?: boolean;
-  id : string;
+  id: string;
   displayName?: string;
   email: string;
 }
@@ -24,29 +24,46 @@ export interface userCredentials {
 })
 export class AuthenticationService {
   currentUser = JSON.parse(localStorage.getItem('user')) as IUserAuthentication;
-  private _currentUser$ = new BehaviorSubject<boolean>(!!(this.currentUser && this.currentUser.token));
-  private _isAdmin$ = new BehaviorSubject<boolean>(this.currentUser ? this.currentUser.user.isAdmin : false);
+  private _currentUser$ = new BehaviorSubject<boolean>(
+    !!(this.currentUser && this.currentUser.token)
+  );
+  private _isAdmin$ = new BehaviorSubject<boolean>(
+    this.currentUser ? this.currentUser.user.isAdmin : false
+  );
 
   rootUrl = `https://movie-app-v1.herokuapp.com/api/auth`;
-  constructor(private _http: HttpClient) { }
+  constructor(private _http: HttpClient) {}
   signUp(userData: userCredentials) {
-    return this._http.post<IUserAuthentication>(`${this.rootUrl}/signUp`, userData)
-    .pipe(
-      tap((resp) => resp.token ? this._currentUser$.next(true) : this._currentUser$.next(false)),
-      tap((resp) => resp.user.isAdmin ? this._isAdmin$.next(true) : this._isAdmin$.next(false))
-    );
+    return this._http
+      .post<IUserAuthentication>(`${this.rootUrl}/signUp`, userData)
+      .pipe(
+        tap((resp) =>
+          resp.token
+            ? this._currentUser$.next(true)
+            : this._currentUser$.next(false)
+        ),
+        tap((resp) =>
+          resp.user.isAdmin
+            ? this._isAdmin$.next(true)
+            : this._isAdmin$.next(false)
+        )
+      );
   }
-  
+
   login(username: string, password: string): Observable<IUserAuthentication> {
-   return this._http.post<IUserAuthentication>(`${this.rootUrl}/signin`, { email: username, password })
-   .pipe(
-     tap((resp) => resp.token && this._currentUser$.next(true)),
-     tap((resp) => resp.user.isAdmin && this._isAdmin$.next(true))
-   )
+    return this._http
+      .post<IUserAuthentication>(`${this.rootUrl}/signin`, {
+        email: username,
+        password,
+      })
+      .pipe(
+        tap((resp) => resp.token && this._currentUser$.next(true)),
+        tap((resp) => resp.user.isAdmin && this._isAdmin$.next(true))
+      );
   }
 
   isAuthenticated(): Observable<boolean> {
-    return this._currentUser$
+    return this._currentUser$;
   }
 
   isAuthorized() {
@@ -55,7 +72,8 @@ export class AuthenticationService {
 
   setAuthenticationState(state: boolean) {
     if (state == false) {
-      localStorage.removeItem('user')
+      localStorage.removeItem('user');
+      this._isAdmin$.next(state);
     }
     this._currentUser$.next(state);
   }
